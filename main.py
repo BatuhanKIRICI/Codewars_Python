cc58cd634217f9eec85e6acdfea38746ac8ab9ce
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-10-16 17:51:43 +0200

    "HR_ Runner_up"

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-i = 4
-d = 4.0
-s = "HackerRank "
-# ------------------------------------------
-a = int(5.0)
-b = float(5.2)
-c = "Hello!"
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
 
+    def runner_up(item):
+        unique_arr = list(set(sorted((item), reverse=True)))
 
-print(i + a)
-print(d + b)
-print(s + c)
+        if len(unique_arr) < 2:
+            return None
+
+        unique_arr.sort(reverse=True)
+
+        return unique_arr[1]
+
+    print(runner_up(arr))
